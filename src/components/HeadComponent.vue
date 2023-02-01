fcb8e67912cc59e69f1e2b14a9f14dcba897e1d4
wooyeol <dnduf158@gmail.com>
2023-02-01 19:39:53 +0900

    fix : 헤드 컴포넌트 수정

--- src/components/HeadComponent.vue
+++ src/components/HeadComponent.vue
@@ -1,4 +1,4 @@
-<template lang="">
+<template>
   <div class="header"></div>
 </template>
 
@@ -11,16 +11,15 @@ export default {};
   position: relative;
   background-color: #1c9c85;
   color: aliceblue;
-  height: 5rem;
+  height: 10vh;
 }
-
 .header::before {
   position: absolute;
   bottom: 0;
   left: 0;
   content: "";
   width: 100%;
-  height: 2rem;
+  height: 4vh;
   background-color: white;
   border-radius: 2rem 2rem 0 0;
 }
