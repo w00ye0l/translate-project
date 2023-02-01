52720d77d9bf682f7b426482497eac5488e9946f
wooyeol <dnduf158@gmail.com>
2023-02-01 22:47:09 +0900

    fix : 자식 컴포넌트로 데이터 전송 및 전달받은 데이터 수정

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view
+      v-on:selectCountry="selectCountry"
+      v-on:selectPlace="selectPlace"
+      v-bind:propsCountry="country"
+      v-bind:propsPlace="place"
+    >
+    </router-view>
+    <!-- <p>{{ country }} | {{ place }}</p> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      country: "",
+      place: "",
+    };
+  },
+  methods: {
+    selectCountry(country) {
+      this.country = country;
+      localStorage.setItem("country", this.country);
+      localStorage.removeItem("place");
+      // console.log("현재 저장된 나라 " + this.country);
+    },
+    selectPlace(place) {
+      this.place = place;
+      localStorage.setItem("place", this.place);
+      // console.log("현재 저장된 장소 " + this.place);
+    },
+  },
 };
 </script>
 
