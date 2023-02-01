3332f37329123f18e7e1825cff02b2cbcf230585
wooyeol <dnduf158@gmail.com>
2023-02-01 19:40:10 +0900

    add : 네브 컴포넌트 추가

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav">
+    <div>
+      <p>화살표</p>
+    </div>
+    <img class="logo" src="../assets/logo.png" alt="" />
+    <div>
+      <p>더보기</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nav {
+  position: absolute;
+  left: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5rem;
+  transform: translate(-50%, -20%);
+}
+.logo {
+  width: 5rem;
+  height: 5rem;
+}
+</style>
