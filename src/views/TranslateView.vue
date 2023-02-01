4b66d2ec0df159b5cf9f718c3ad60d38b2d176f4
wooyeol <dnduf158@gmail.com>
2023-02-01 22:46:04 +0900

    add : 나라/장소 선택 후 데이터 전달

---
+++ src/views/TranslateView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <HeadComponent></HeadComponent>
+    <NavComponent></NavComponent>
+    <div class="main">
+      <div class="translate__div">
+        <p>{{ propsCountry }} | {{ propsPlace }}</p>
+      </div>
+      <!-- <p>{{ propsCountry }} | {{ propsPlace }}</p> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import HeadComponent from "@/components/HeadComponent.vue";
+import NavComponent from "@/components/NavComponent.vue";
+
+export default {
+  name: "TranslateView",
+  props: ["propsCountry", "propsPlace"],
+  data: () => ({}),
+  components: {
+    HeadComponent,
+    NavComponent,
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.main {
+  margin: 2rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.translate__div {
+  margin: 1rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 18rem;
+  height: 5rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.12);
+  border-radius: 1rem;
+}
+</style>
