c061667cf2fca686bd67e5febd2f121f6a355cb4
wooyeol <dnduf158@gmail.com>
2023-02-02 19:43:04 +0900

    add : axios 추가, 파파고 API 설정

--- src/views/TranslateView.vue
+++ src/views/TranslateView.vue
@@ -6,7 +6,10 @@
       <div class="translate__div">
         <p>{{ country }} | {{ place }}</p>
       </div>
-      <!-- <p>{{ propsCountry }} | {{ propsPlace }}</p> -->
+      <div class="translate__div">
+        <!-- <p>{{ result }}</p> -->
+        <p>result</p>
+      </div>
     </div>
   </div>
 </template>
@@ -14,6 +17,7 @@
 <script>
 import HeadComponent from "@/components/HeadComponent.vue";
 import NavComponent from "@/components/NavComponent.vue";
+import QueryString from "qs";
 
 export default {
   name: "TranslateView",
@@ -21,16 +25,52 @@ export default {
   data: () => ({
     country: "",
     place: "",
+    result: "",
   }),
   created() {
     this.country = sessionStorage.getItem("country");
     this.place = sessionStorage.getItem("place");
+    // this.getData();
   },
   components: {
     HeadComponent,
     NavComponent,
   },
-  methods: {},
+  methods: {
+    async getData() {
+      const params = QueryString.stringify({
+        source: "ko",
+        target: "en",
+        text: "안녕하세요.",
+      });
+
+      const config = {
+        baseURL: "https://proxy.cors.sh/https://openapi.naver.com/v1",
+        headers: {
+          "x-cors-api-key": "temp_0a5af26e928255d482e7a5efdebeecca",
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "X-Naver-Client-Id": "2Rql9F4A4qbEI2DhUe2i",
+          "X-Naver-Client-Secret": "QGlM99Zhql",
+        },
+      };
+
+      try {
+        await this.$axios
+          .post("/papago/n2mt", params, config)
+          .then((res) => {
+            if (res.status == 200) {
+              console.log(res.data.message.result.translatedText);
+              this.result = res.data.message.result.translatedText;
+            }
+          })
+          .catch((error) => {
+            console.log("실패", error);
+          });
+      } catch (error) {
+        console.log("실패", error);
+      }
+    },
+  },
 };
 </script>
 
