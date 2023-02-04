ebaf6c2f8b82725325664c0ab9bb0579eb8a2ef6
wooyeol <dnduf158@gmail.com>
2023-02-05 00:30:35 +0900

    fix : regular 아이콘 삭제

--- src/main.js
+++ src/main.js
@@ -7,9 +7,8 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
 
-library.add(fas, far);
+library.add(fas);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
