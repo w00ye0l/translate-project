923b662b292385ecfd56f5b572f3816edc6fbf6e
wooyeol <dnduf158@gmail.com>
2023-02-07 02:10:16 +0900

    fix : 장소별 아이콘 추가 및 직접 입력 링크 분리

--- src/views/PlaceView.vue
+++ src/views/PlaceView.vue
@@ -3,13 +3,26 @@
     <HeadComponent></HeadComponent>
     <NavComponent></NavComponent>
     <div class="main">
-      <div
-        class="place__div"
-        v-for="place in $store.state.places"
-        v-bind:key="place"
-        v-on:click="selectPlace($event)"
-      >
-        <p>{{ place }}</p>
+      <div class="place__container">
+        <div
+          class="place__div select"
+          v-for="(icon, place) in $store.state.places"
+          v-bind:key="place"
+          v-on:click="selectPlace($event)"
+        >
+          <div class="icon__container">
+            <font-awesome-icon
+              class="place__icon"
+              :icon="['fas', icon]"
+              size="xl"
+            />
+          </div>
+          <p class="place__name">{{ place }}</p>
+        </div>
+      </div>
+
+      <div class="place__div self" v-on:click="translateSelf()">
+        <p class="place__name">직접 입력</p>
       </div>
     </div>
   </div>
@@ -28,9 +41,12 @@ export default {
   },
   methods: {
     selectPlace(event) {
-      const text = event.target.innerText;
+      const text = event.currentTarget.querySelector("p").innerText;
       console.log(text);
       this.$emit("selectPlace", text);
+      this.$router.push("translate");
+    },
+    translateSelf() {
       this.$router.push("translate");
     },
   },
@@ -39,20 +55,78 @@ export default {
 
 <style scoped>
 .main {
-  margin: 2rem 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
+  margin: 2rem auto;
+  width: 70vw;
+  max-width: 1200px;
+}
+.place__container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+  gap: 0 5vw;
 }
 .place__div {
-  margin: 1rem 0;
+  margin: 1rem auto;
+  padding: 0.5rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  background-color: #00a886;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  border-radius: 1rem;
+}
+.select {
+  width: 100%;
+  max-width: 20rem;
+}
+.self {
+  width: 100%;
+  max-width: 700px;
+  height: 5rem;
+}
+.place__div:hover {
+  cursor: pointer;
+}
+.icon__container {
+  margin-bottom: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 18rem;
-  height: 5rem;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.12);
-  border-radius: 1rem;
+  width: 4rem;
+  height: 4rem;
+  background-color: #fff;
+  border-radius: 50%;
+}
+.place__icon {
+  color: #005c3e;
+  font-size: 2rem;
+}
+.place__name {
+  font-size: 1.3rem;
+}
+
+@media (min-width: 992px) {
+  .place__icon {
+    font-size: 3rem;
+  }
+  .icon__container {
+    width: 5rem;
+    height: 5rem;
+  }
+  .self {
+    height: 7rem;
+  }
+  .place__name {
+    font-size: 1.5rem;
+  }
+}
+
+@media (max-width: 576px) {
+  .place__name {
+    font-size: 5vw;
+  }
 }
 </style>
