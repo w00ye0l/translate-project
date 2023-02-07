649d962af589229f27603cf29af162f7cc10e134
wooyeol <dnduf158@gmail.com>
2023-02-08 01:48:14 +0900

    fix : 개발자 탭에서 탭버튼 안보이도록 수정

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -9,17 +9,30 @@
       />
       <!-- <font-awesome-icon class="fa-2x" icon="fa-arrow-left" /> -->
     </div>
+
     <img class="logo" src="../assets/images/logo.png" alt="" />
-    <div>
+
+    <div v-on:click="$router.push('developer')">
       <!-- <font-awesome-icon class="fa-2x" icon="fa-bars" /> -->
-      <font-awesome-icon class="nav-icon" :icon="['fas', 'bars']" size="xl" />
+      <font-awesome-icon
+        v-show="currentRouteName !== 'developer'"
+        class="nav-icon"
+        :icon="['fas', 'bars']"
+        size="xl"
+      />
       <!-- <font-awesome-icon icon="fa-solid fa-bars" /> -->
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    currentRouteName() {
+      return this.$route.name;
+    },
+  },
+};
 </script>
 
 <style scoped>
