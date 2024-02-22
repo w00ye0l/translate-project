27611f0cc5b9e8429f2fdddce09b1066099725fc
wooyeol <dnduf158@gmail.com>
2024-02-22 19:58:55 +0900

    fix : api 프록시 설정

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
         target: "https://openapi.naver.com/v1/:path*",
         changeOrigin: true,
         headers: {
-          "Access-Control-Allow-Origin": "*",
           "X-Naver-Client-Id": "2Rql9F4A4qbEI2DhUe2i",
           "X-Naver-Client-Secret": "QGlM99Zhql",
         },
